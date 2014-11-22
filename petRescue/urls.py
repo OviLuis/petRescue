b41019cf79c50fff0ab0208d36e34a93335a1d30
Luis Gonzalez <luis.ovidio.gonzalez@correounivalle.edu.co>
2014-11-22 15:05:58 -0500

    adelanto de  vistas y templates

--- petRescue/urls.py
+++ petRescue/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'petRescue.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'principal.views.inicio', ),
+    url(r'^perdidos/$', 'principal.views.perdidos', name='perdidos'),
+    url(r'^encontrados/$', 'principal.views.encontrados', name='encontrados'),
+    url(r'^adopciones/$', 'principal.views.adopcion', name='adopciones'),
+
+
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from principal.models import *
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def inicio(request):
+	return render_to_response('inicio.html', context_instance = RequestContext(request))
+
+def perdidos(request):
+	return render_to_response ('perdidos.html', context_instance = RequestContext(request) )
+
+def encontrados(request):
+	return render_to_response ('encontrados.html', context_instance = RequestContext(request))
+
+def adopcion(request):
+	return render_to_response ('adopcion.html', context_instance = RequestContext(request));
