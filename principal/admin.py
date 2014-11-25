020bace331f941d9ec6ce76ddaa698f53768bf6b
Luis Gonzalez <luis.ovidio.gonzalez@correounivalle.edu.co>
2014-11-25 16:37:04 -0500

    Se borro directorio migrations; se actualizaron los modelos

--- principal/admin.py
+++ principal/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from  principal.models import Mascota
+from  principal.models import *
 # Register your models here.
 
 admin.site.register(Mascota)
+admin.site.register(Perdidos)
+admin.site.register(Encontrados)
+admin.site.register(Adopciones)
--- principal/models.py
+++ principal/models.py
@@ -4,15 +4,43 @@ from django.db import models
 
 class Mascota(models.Model):
 	nombre = models.CharField(max_length=50)
-	raza = models.CharField(max_length=50)
-	descripcion = models.CharField(max_length=400)
-	edad = models.IntegerField();
+	raza = models.CharField(max_length=50)	
+	edad = models.IntegerField()
+	especie = models.CharField(max_length=50)
 	sexo = models.CharField(max_length=10)
-	ubicacion = models.CharField(max_length=100)
 	foto = models.ImageField(verbose_name= 'foto')
+	descripcion = models.CharField(max_length=400)
+	
 
 	def  __unicode__(self):
 		return u'%s %s' % (self.id, self.nombre)
+
+
+class Perdidos(Mascota):
+	fechaDesaparicion = models.DateTimeField(auto_now=True)
+	dirDesaparicion = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return u'%s' % (self.id)
+
+
+class Encontrados(Mascota):
+	fechaEncuentro = models.DateTimeField()
+	dirEncuentro = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return u'%s' % (self.id)
+
+
+class Adopciones(Mascota):
+	direccion = models.CharField(max_length=59)
+
+	def __unicode__(self):
+		return u'%s' % (self.id)
+
+
+
+		
 
 
 	
