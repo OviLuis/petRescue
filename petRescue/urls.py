a75349513ce7b1d1ba9395f9bd3beec60137a4d4
donberna <donberna-93@hotmail.com>
2014-11-28 18:02:43 -0500

    estandar

--- petRescue/urls.py
+++ petRescue/urls.py
@@ -6,8 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'petRescue.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('principal.urls')),
-    url(r'^', include('web.urls',namespace='web')),
+    url(r'^', include('web.urls', namespace='web')),
 )
