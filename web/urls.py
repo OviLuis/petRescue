a5cce67aefeec81ef4d9c7afcbf6395873afe0c9
donberna <donberna-93@hotmail.com>
2014-11-28 09:45:42 -0500

    vista home

--- web/urls.py
+++ web/urls.py
@@ -4,16 +4,16 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 
-	url(r'^$','web.views.inicio', name='inicio' ),
-	url(r'^perdidos/$', 'web.views.perdidos', name='perdidos'),
-    url(r'^reportarPerdido/$', 'web.views.reportarPerdido', name='reportarPerdido'),
-    url(r'^encontrados/$', 'web.views.encontrados', name='encontrados'),
-    url(r'^reportarEncontrado/$', 'web.views.reportarEncontrado', name='reportarEncontrado'),
-    url(r'^adopciones/$', 'web.views.adopcion', name='adopciones'),
-    url(r'^reportarAdopcion/$', 'web.views.reportarAdopcion', name='reportarAdopcion'),
-    url(r'^registro/$', 'web.views.registro', name='registro'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+  url(r'^$','web.views.inicio', name='inicio' ),
+  url(r'^home/$', 'web.views.home', name='home'),
+  url(r'^perdidos/$', 'web.views.perdidos', name='perdidos'),
+  url(r'^reportarPerdido/$', 'web.views.reportarPerdido', name='reportarPerdido'),
+  url(r'^encontrados/$', 'web.views.encontrados', name='encontrados'),
+  url(r'^reportarEncontrado/$', 'web.views.reportarEncontrado', name='reportarEncontrado'),
+  url(r'^adopciones/$', 'web.views.adopcion', name='adopciones'),
+  url(r'^reportarAdopcion/$', 'web.views.reportarAdopcion', name='reportarAdopcion'),
+  url(r'^registro/$', 'web.views.registro', name='registro'),
+  url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
        {'document_root':settings.MEDIA_ROOT,}
        ),
-
  )
--- web/views.py
+++ web/views.py
@@ -10,71 +10,69 @@ from principal.models import *
 
 # Create your views here.
 
+
 def inicio(request):
-	return render_to_response('inicio.html', context_instance = RequestContext(request))
+    return render_to_response('inicio.html', context_instance = RequestContext(request))
 
 
 def perdidos(request):
-	perdidos = Perdidos.objects.all()
-	return render_to_response('perdidos.html',{'perdidos':perdidos},context_instance=RequestContext(request))
+    perdidos = Perdidos.objects.all()
+    return render_to_response('perdidos.html',{'perdidos':perdidos},context_instance=RequestContext(request))
 
 
 def reportarPerdido(request):
-	if request.method == 'POST':
-		formulario = PerdidosForm(request.POST, request.FILES)
-		if formulario.is_valid():
-			formulario.save()
-			return HttpResponseRedirect('/perdidos')
-	else:
-		formulario = PerdidosForm()
+    if request.method == 'POST':
+        formulario = PerdidosForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/perdidos')
+    else:
+        formulario = PerdidosForm()
 
-	return render_to_response('perdidosform.html',{'formulario':formulario},context_instance=RequestContext(request))
+    return render_to_response('perdidosform.html',{'formulario':formulario},context_instance=RequestContext(request))
 
 
 
 def encontrados(request):
-	return render_to_response ('encontrados.html', context_instance = RequestContext(request))
+    return render_to_response ('encontrados.html', context_instance = RequestContext(request))
 
 
 
 def reportarEncontrado(request):
-	if request.method == 'POST':
-		formulario = EncontradosForm(request.POST, request.FILES)
-		if formulario.is_valid():
-			formulario.save()
-			return HttpResponseRedirect('/encontrados')
-	else:
-		formulario = EncontradosForm()
+    if request.method == 'POST':
+        formulario = EncontradosForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/encontrados')
+    else:
+        formulario = EncontradosForm()
 
-	return render_to_response('encontradosform.html',{'formulario':formulario},context_instance=RequestContext(request))
+    return render_to_response('encontradosform.html',{'formulario':formulario},context_instance=RequestContext(request))
 
 
 def adopcion(request):
-	return render_to_response ('adopcion.html', context_instance = RequestContext(request));
+    return render_to_response ('adopcion.html', context_instance = RequestContext(request));
 
 
 def reportarAdopcion(request):
-	if request.method == 'POST':
-		formulario = AdopcionesForm(request.POST, request.FILES)
-		if formulario.is_valid():
-			formulario.save()
-			return HttpResponseRedirect('/adopcion')
-	else:
-		formulario = AdopcionesForm()
+    if request.method == 'POST':
+        formulario = AdopcionesForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/adopcion')
+    else:
+        formulario = AdopcionesForm()
 
-	return render_to_response('adopcionesform.html',{'formulario':formulario},context_instance=RequestContext(request))
+    return render_to_response('adopcionesform.html',{'formulario':formulario},context_instance=RequestContext(request))
 
 
-
-def registro(request):
-	usuario = request.user
-	
-	if request.method=='POST':
-		formulario = UsuarioForm(request.POST)
-		if formulario.is_valid:
-			formulario.save()
-			return HttpResponseRedirect('/')
-			
-	else:
-		formulario = UsuarioForm()
-	return render_to_response ('nuevousuario.html',{'formulario':formulario, 'usuario':usuario}, context_instance=RequestContext(request))
+def home(request):
+    usuario = request.user
+    if request.method=='POST':
+        formulario = UsuarioForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = UsuarioForm()
+    return render_to_response ('home.html',{'formulario':formulario, 'usuario':usuario}, context_instance=RequestContext(request))
