2cceab90c8907124aa06a5f85fb47f8d662f26eb
donberna <donberna-93@hotmail.com>
2014-11-28 12:49:23 -0500

    vista home

--- principal/forms.py
+++ principal/forms.py
@@ -1,32 +1,39 @@
+# -*- coding: utf-8 -*-
+
 from django.forms import ModelForm
 from django import forms
 from principal.models import *
 
-
 class MascotaForm(ModelForm):
-	class Meta:
-			model = Mascota
+    class Meta:
+            model = Mascota
 
 
 class PerdidosForm(ModelForm):
-	class Meta:
-			model = Perdidos
+    class Meta:
+            model = Perdidos
 
 
 class EncontradosForm(ModelForm):
-	class Meta:
-			model = Encontrados
+    class Meta:
+            model = Encontrados
 
 
 
 class AdopcionesForm(ModelForm):
-	class Meta:
-			model = Adopciones
+    class Meta:
+            model = Adopciones
 
 
 class UsuarioForm(ModelForm):
-	"""docstring for UsuarioForm"""
-	class Meta:
-		"""docstring for Meta"""
-		model = Usuario
-		fields = ('email', 'nombre', 'direccion', 'telefono', 'password')
+    nombre = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}))
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Correo Electronico'}))
+    direccion = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Direccion'}))
+    telefono = forms.IntegerField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Numero Telefonico'}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirmar Contraseña'}))
+
+
+    class Meta: 
+        model = Usuario
+        fields = []
