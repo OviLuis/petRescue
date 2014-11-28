72d59342b70b3700e43bf6ace487fb21dd567a55
rizotas <aslheyramirez@gmail.com>
2014-11-28 11:27:56 -0500

    registro (funcional) de perdidos, adopcion, encontrados

--- web/static/js/adopcion.js
+++ web/static/js/adopcion.js
@@ -1,33 +1,21 @@
-function getDatosAdopcion()
-{
-	var form_adopcion = {
-		nombre: $("#id_nombre").val(),
-		raza: $("#id_raza").val(),
-		edad: $("#id_edad").val(),
-		especie: $("#id_especie").val(),
-		sexo: $("#id_sexo").val(),
-		descripcion: $("#id_descripcion").val(),
-		direccion: $("#id_direccion").val(),
-		foto: $("#id_foto").val()
-		}
-	console.log(form_adopcion)
-	return form_adopcion;
-}
+function sendDatosAdopcion() {
+	//obtiene el csrftoken
+	var csrftoken = $.cookie('csrftoken');
+	//url de la peticion
+	url = 'http://'+window.location.host+'/api/adopcion/'
 
+	//configura el csrftoken a la peticion ajax
+	$.ajaxSetup({
+    	beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    	}
+	});
 
-function sendRequest (data) {
-	//http://192.168.14.45/ajax2/server/main.php
-	/*$.get("main.php", {lang: data}, function(response){
-		$("#gretting").text(response)
-	});*/
-
-	url = 'http://'+window.location.host+'/api/perdido/'
-	console.log(url)
-
+	//peticion ajax
 	$.ajax({
 		type: 'POST',
 		url: url,
-		data: data
+		data: $("#formulario_adopcion").serialize()
 	})
 	.done(function(response){
 		//$("#gretting").text(response)
@@ -46,11 +34,9 @@ function sendRequest (data) {
 
 
 $(document).ready(function(){
-	$("#test_adopcion").click(function(){
-	//$("#repotar_perdido_btn").click(function(){
-		
-		//sendRequest(getDatosPerdido());
-		getDatosAdopcion()
-	});
+	$("#formulario_adopcion").submit(function(){
+        sendDatosAdopcion();
+    });
+
 
 });
