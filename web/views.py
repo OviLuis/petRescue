436161f5fc0ba4c199720330d573f51efc9bf419
rizotas <aslheyramirez@gmail.com>
2014-11-30 23:00:36 -0500

    fix login

--- web/views.py
+++ web/views.py
@@ -15,7 +15,7 @@ from principal.models import *
 
 
 def home(request):
-    return render_to_response('index.html', {'loguin_form': loguin_form}, context_instance=RequestContext(request))
+    return render_to_response('index.html', {'loguin_form': loguinForm()}, context_instance=RequestContext(request))
 
 
 def perdidos(request):
