51ff4bc8ca932f2d66f1bb8449c3e8f65ecbfeb8
donberna <donberna-93@hotmail.com>
2014-12-02 17:30:59 -0500

    hola

--- web/static/js/encontrado.js
+++ web/static/js/encontrado.js
@@ -5,7 +5,7 @@ function getDatosEncontrados()
         type : "GET",   
         success : function(json) {
             console.log("completo");
-			creaDivs(json);
+			creaDivs(json,"encontrados");
 		
         },
         error : function(xhr,errmsg,err) {
